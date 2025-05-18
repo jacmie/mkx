fa25b8f5c96637bc2b139564b77eca947723b8ef
jacmie <mieloszyk.jacek@gmail.com>
2025-05-18 11:17:33 +0200

    hid ble improved

--- mkx/hid_ble.py
+++ mkx/hid_ble.py
@@ -1,12 +1,25 @@
-import usb_hid
+from adafruit_ble import BLERadio
+from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
+from adafruit_ble.services.standard.hid import HIDService
 from adafruit_hid.keyboard import Keyboard
-
 from mkx.hid_abstract import HID_abstract
 
 
 class HID_BLE(HID_abstract):
     def __init__(self):
-        self.keyboard = Keyboard(usb_hid.devices)
+        self.ble = BLERadio()
+        self.hid = HIDService()
+        self.advertisement = ProvideServicesAdvertisement(self.hid)
+
+        print("Starting BLE advertisement...")
+        self.ble.start_advertising(self.advertisement)
+
+        # Wait for connection
+        while not self.ble.connected:
+            pass
+
+        print("BLE connected")
+        self.keyboard = Keyboard(self.hid.devices)
 
     def send_key(self, keycode, pressed):
         if pressed:
