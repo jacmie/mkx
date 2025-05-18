4d8af55518eaa5ceec98f18230a7c5c14ad14a8e
jacmie <mieloszyk.jacek@gmail.com>
2025-05-18 11:27:28 +0200

    hid ble improved

--- mkx/hid_ble.py
+++ mkx/hid_ble.py
@@ -1,3 +1,5 @@
+import sys
+
 from adafruit_ble import BLERadio
 from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
 from adafruit_ble.services.standard.hid import HIDService
@@ -7,19 +9,25 @@ from mkx.hid_abstract import HID_abstract
 
 class HID_BLE(HID_abstract):
     def __init__(self):
-        self.ble = BLERadio()
-        self.hid = HIDService()
-        self.advertisement = ProvideServicesAdvertisement(self.hid)
+        try:
+            self.ble = BLERadio()
+            self.hid = HIDService()
+            self.advertisement = ProvideServicesAdvertisement(self.hid)
 
-        print("Starting BLE advertisement...")
-        self.ble.start_advertising(self.advertisement)
+            print("Starting BLE advertisement...")
+            self.ble.start_advertising(self.advertisement)
 
-        # Wait for connection
-        while not self.ble.connected:
-            pass
+            # Wait for connection
+            while not self.ble.connected:
+                pass
 
-        print("BLE connected")
-        self.keyboard = Keyboard(self.hid.devices)
+            print("BLE connected")
+            self.keyboard = Keyboard(self.hid.devices)
+
+        except (ImportError, RuntimeError) as e:
+            print("BLE initialization failed:", e)
+            print("This board may not support BLE or the firmware lacks BLE support.")
+            sys.exit()
 
     def send_key(self, keycode, pressed):
         if pressed:
