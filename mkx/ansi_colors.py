0ed6b77f50888927353ad2300214d32b689c89c4
jacmie <mieloszyk.jacek@gmail.com>
2026-02-08 11:16:00 +0100

    Added ansi colors for output higlight

---
+++ mkx/ansi_colors.py
@@ -0,0 +1,61 @@
+class Ansi:
+    # Reset / styles
+    RESET = "\033[0m"
+    BOLD = "\033[1m"
+    DIM = "\033[2m"
+    UNDERLINE = "\033[4m"
+    REVERSE = "\033[7m"
+
+    # Standard foreground colors
+    BLACK = "\033[30m"
+    RED = "\033[31m"
+    GREEN = "\033[32m"
+    YELLOW = "\033[33m"
+    BLUE = "\033[34m"
+    MAGENTA = "\033[35m"
+    CYAN = "\033[36m"
+    WHITE = "\033[37m"
+    GRAY = "\033[90m"
+
+    # Bright foreground colors
+    BRIGHT_BLACK = "\033[90m"
+    BRIGHT_RED = "\033[91m"
+    BRIGHT_GREEN = "\033[92m"
+    BRIGHT_YELLOW = "\033[93m"
+    BRIGHT_BLUE = "\033[94m"
+    BRIGHT_MAGENTA = "\033[95m"
+    BRIGHT_CYAN = "\033[96m"
+    BRIGHT_WHITE = "\033[97m"
+
+    # Background colors
+    BG_BLACK = "\033[40m"
+    BG_RED = "\033[41m"
+    BG_GREEN = "\033[42m"
+    BG_YELLOW = "\033[43m"
+    BG_BLUE = "\033[44m"
+    BG_MAGENTA = "\033[45m"
+    BG_CYAN = "\033[46m"
+    BG_WHITE = "\033[47m"
+
+    # Bright background colors
+    BG_BRIGHT_BLACK = "\033[100m"
+    BG_BRIGHT_RED = "\033[101m"
+    BG_BRIGHT_GREEN = "\033[102m"
+    BG_BRIGHT_YELLOW = "\033[103m"
+    BG_BRIGHT_BLUE = "\033[104m"
+    BG_BRIGHT_MAGENTA = "\033[105m"
+    BG_BRIGHT_CYAN = "\033[106m"
+    BG_BRIGHT_WHITE = "\033[107m"
+
+
+class Ansi256:
+    MINT = "\033[38;5;121m"  # light green / mint
+    LIGHT_GREEN = "\033[38;5;120m"
+    PINK = "\033[38;5;213m"
+    HOT_PINK = "\033[38;5;205m"
+    ORANGE = "\033[38;5;208m"
+    LIGHT_ORANGE = "\033[38;5;214m"
+    SALMON = "\033[38;5;209m"
+    PEACH = "\033[38;5;215m"
+    LAVENDER = "\033[38;5;183m"
+    SKY = "\033[38;5;117m"
